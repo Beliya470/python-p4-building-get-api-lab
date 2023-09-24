202e9251aa236c669776ab2ffc691678e8ba80b1
Beliya470 <anneanziay78@gmail.com>
2023-09-24 23:39:58 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -1,38 +1,37 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify
-from flask_migrate import Migrate
-
+from flask import Flask, jsonify
 from models import db, Bakery, BakedGood
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
-def index():
-    return '<h1>Bakery GET API</h1>'
+def home():
+    return 'Hello, World!'
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return jsonify([bakery.to_dict() for bakery in bakeries])
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    return jsonify(bakery.to_dict(include=['baked_goods']))
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([good.to_dict() for good in goods])
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(good.to_dict()) if good else jsonify({'error': 'No baked goods found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,20 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
-
     id = db.Column(db.Integer, primary_key=True)
-
+    name = db.Column(db.String(50), nullable=False)
+    baked_goods = db.relationship('BakedGood', backref='bakery', lazy=True)
+    serialize_only = ('id', 'name')  # Only serialize these fields
+    
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
-
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'), nullable=False)
+    serialize_only = ('id', 'name', 'price', 'bakery_id')  # Only serialize these fields
